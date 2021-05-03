7070f25b0ce9c4dea7167af060078335b677b80e
contactLost <50428210+contactLost@users.noreply.github.com>
2021-05-03 23:58:49 +0300

    final raport upload

--- GroupRequestRepository.java
+++
@@ -1,7 +0,0 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.People.*;
-import java.util.List;
-public class GroupRequestRepository extends JPARepostiory<GroupRequest, Long>{
-	List<GroupRequest> findByGroupOrderByDate(Group group);
-	List<GroupRequest> findByStudentOrderByDate(People student);
-}
