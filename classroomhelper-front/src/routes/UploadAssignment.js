1b97e071e2125717ad5ea96f756665d37c242ac1
contactLost <50428210+contactLost@users.noreply.github.com>
2021-05-06 02:05:21 +0300

    before apply dark palette

--- classroomhelper-front/src/routes/UploadAssignment.js
+++ classroomhelper-front/src/routes/UploadAssignment.js
@@ -3,39 +3,58 @@ import "./Home.css"
 import {Link} from "react-router-dom";
 
 export default class UploadAssignment extends React.Component {
-    componentWillMount() {
+
+    state = {
+        numberOfCriteria: 0
+    }
+
+    componentDidMount() {
         this.props.history.push('/UploadAssignment');
     }
 
+    handleUploadClick(){
 
+    }
+
+
+    handleAddCriteriaButton(){
+  
+    }
 
     render() {
+
         return (
-            <div className="Home">
-                <header>
-                    <p>
-                        Upload Assignment
-              </p>
-                </header>
-                <form>
-                    <label>
-                        Description:
-                  <input type="text" name="Description" />
-                    </label>
-                </form>
+            <div class="grid-container">
+                <div class="column">
+                            <p>
+                                Upload Assignment
+                            </p>
 
-                <form>
-                    <label>
-                        Due Date:
-                  <input type="text" name="Due Date" />
-                    </label>
-                </form>
+                        <form>
+                            <label>
+                                Description:
+                        <input type="text" name="Description" />
+                            </label>
+                        </form>
 
+                        <form>
+                            <label>
+                                Due Date:
+                        <input type="text" name="Due Date" />
+                            </label>
+                        </form>
 
-                <Link to="/Assignment">  <button>Upload</button>   </Link>
+            
+                        <Link to="/Assignment">  <button onClick={this.handleUploadClick}>Upload</button>   </Link>
+
+                </div>
+
+                <div class="column">
+                        <button class="button" onClick={this.handleAddCriteriaButton}> Add Criteria</button>
+                </div>
 
             </div>
         )
-            ;
+            
     }
 }
