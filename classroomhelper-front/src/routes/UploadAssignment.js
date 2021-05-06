674411091c9ceaf021eadc2d43a2dfe2b07e91e2
contactLost <50428210+contactLost@users.noreply.github.com>
2021-05-06 03:15:12 +0300

    upload assignment page

--- classroomhelper-front/src/routes/UploadAssignment.js
+++ classroomhelper-front/src/routes/UploadAssignment.js
@@ -3,25 +3,73 @@ import "./Home.css"
 import {Link} from "react-router-dom";
 
 export default class UploadAssignment extends React.Component {
+    
+    constructor(){
+        super()
+        this.handleAddCriteriaButton = this.handleAddCriteriaButton.bind(this)
+        this.handleUploadClick = this.handleUploadClick.bind(this)
+    }
 
     state = {
-        numberOfCriteria: 0
+        numberOfCriterias: 0,
+        criteriaArray: []
     }
 
     componentDidMount() {
         this.props.history.push('/UploadAssignment');
     }
 
-    handleUploadClick(){
+    //When pressed upload sends data to API
+    async handleUploadClick(){
 
     }
 
-
+    //Takes values from text boxes and adds them to state
     handleAddCriteriaButton(){
-  
+        var criteriaInput = document.getElementsByClassName("NameField")[0].value
+        var weightInput = document.getElementsByClassName("WeightField")[0].value
+
+        const newNumberOfCriterias = this.state.numberOfCriterias + 1;
+        const newCriteraArray = this.state.criteriaArray;
+        newCriteraArray.push({"key": newNumberOfCriterias,"criteria":criteriaInput, "weight":weightInput});
+
+        this.setState({
+            numberOfCriterias: newNumberOfCriterias,
+            criteriaArray: newCriteraArray
+        })
     }
+
+    //Deletes the Criteria when pressed on top of it.
+    handleDeleteCriteriaButton(indexToDelete){
+
+        const newCriteraArray = this.state.criteriaArray;
+        newCriteraArray.splice(indexToDelete,1)
+        const newNumberOfCriterias = this.state.numberOfCriterias - 1;
+        this.setState({
+            numberOfCriterias: newNumberOfCriterias,
+            criteriaArray: newCriteraArray
+        })
+    }
+
 
     render() {
+        //Prepare Criterias section
+        var criteriasHTML = []
+
+        for (let i = 0; i < this.state.numberOfCriterias; i++) {
+            criteriasHTML.push(
+            <div key={i}
+            onClick={() => {
+                this.handleDeleteCriteriaButton(i)
+            }} 
+            class="criteriaBox"> 
+            Criteria:{this.state.criteriaArray[i].criteria}
+            <br/>
+            Weight:{this.state.criteriaArray[i].weight}
+            </div>)
+            
+        }
+
 
         return (
             <div class="grid-container">
@@ -45,12 +93,17 @@ export default class UploadAssignment extends React.Component {
                         </form>
 
             
-                        <Link to="/Assignment">  <button onClick={this.handleUploadClick}>Upload</button>   </Link>
+                        <Link to="/Assignment">  <button class="button" onClick={this.handleUploadClick}>Upload</button>   </Link>
 
                 </div>
 
                 <div class="column">
+
+                        <form><label> Criteria: <input className="NameField" type="text" name="Criteria" /> </label> </form>
+                        <form><label> Weight: <input className="WeightField" type="text" name="Weight" /> </label> </form>
                         <button class="button" onClick={this.handleAddCriteriaButton}> Add Criteria</button>
+                        {criteriasHTML}
+
                 </div>
 
             </div>
